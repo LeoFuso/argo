ab70aeb00083cde6ae5f6243355408cf70e23548
Leonardo Fuso Nuzzo <leonardofusonuzzo@gmail.com>
2023-02-20 01:16:57 -0300

    issue-8 ― Spacing issue

--- argo-plugin/src/test/kotlin/io/github/leofuso/argo/plugin/tasks/IDLProtocolTaskTest.kt
+++ argo-plugin/src/test/kotlin/io/github/leofuso/argo/plugin/tasks/IDLProtocolTaskTest.kt
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.condition.OS
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
 import kotlin.io.path.Path
+
 @DisabledOnOs(OS.WINDOWS)
 @DisplayName("IDL: Functional tests related to IDLProtocolTask.")
 class IDLProtocolTaskTest {
